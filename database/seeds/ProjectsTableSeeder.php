8f4e29412a981f8e8bbd60961f5c42d78006a4f7
Santiago Barrios H <santiagobarrios08@gmail.com>
2016-12-16 14:08:46 -0500

    Incremento en los proyectos
    Se modificó la cantidad de creación de datos "fake" para el modelo
    "Project"

--- database/seeds/ProjectsTableSeeder.php
+++ database/seeds/ProjectsTableSeeder.php
@@ -12,6 +12,6 @@ class ProjectsTableSeeder extends Seeder
      */
     public function run()
     {
-      factory(Project::class, 10)->create();
+      factory(Project::class, 20)->create();
     }
 }
