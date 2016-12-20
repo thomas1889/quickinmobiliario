6d7e5d7e19525062d4831889397d1f21dcac394c
Stivenson <stivenson02@gmail.com>
2016-12-20 09:26:10 -0500

    Modificacion de base de datos dejar campos vacios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,22 +4,21 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
-{
+class CreateUsersTable extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('first_name', 150);
             $table->string('last_name', 150);
             $table->string('business_name', 150);
             $table->string('cell_phone', 45);
-            $table->string('address');
+            $table->string('address')->default("");
             $table->string('document_type', 45);
             $table->integer('age');
             $table->string('gender', 10);
@@ -39,8 +38,8 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('users');
     }
+
 }
