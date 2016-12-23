7274a76489783e6b0dbb8195778feab40eb23163
Santiago Barrios H <santiagobarrios08@gmail.com>
2016-12-23 09:41:51 -0500

    Merge  web routes
    inclusion de más rutas

--- routes/web.php
+++ routes/web.php
@@ -1,56 +1,61 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
 Route::get('/', 'HomeController@index');
 
 /**
-* Authorization routes
-*/
+ * Authorization routes
+ */
 Auth::routes();
+Route::get('profile', [
+    'uses' => 'Auth\User@profile',
+    'as' => 'profile_show_perfil'
+]);
 
 /**
-* Property routes
-*/
+ * Property routes
+ */
 Route::get('inmueble/crear', [
-  'uses' => 'PropertyController@create',
-  'as' => 'property_create_path'
+    'uses' => 'PropertyController@create',
+    'as' => 'property_create_path'
 ]);
 Route::post('inmueble/crear', [
-  'uses' => 'PropertyController@store',
-  'as' => 'property_create_path'
+    'uses' => 'PropertyController@store',
+    'as' => 'property_create_path'
 ]);
 Route::get('inmueble/{id}/editar', [
-  'uses' => 'PropertyController@edit',
-  'as' => 'property_edit_path'
+    'uses' => 'PropertyController@edit',
+    'as' => 'property_edit_path'
 ]);
 Route::put('inmueble/{id}/editar', [
-  'uses' => 'PropertyController@update',
-  'as' => 'property_edit_path'
+    'uses' => 'PropertyController@update',
+    'as' => 'property_edit_path'
 ]);
-Route::delete('inmueble/{id}/borrar',[
-  'uses' => 'PropertyController@destroy',
-  'as' => 'property_delete_path'
+Route::delete('inmueble/{id}/borrar', [
+    'uses' => 'PropertyController@destroy',
+    'as' => 'property_delete_path'
 ]);
 Route::get('inmuebles', [
-  'uses' => 'PropertyController@index',
-  'as' => 'properties_path'
+    'uses' => 'PropertyController@index',
+    'as' => 'properties_path'
 ]);
 Route::get('inmueble/{id}', [
-  'uses' => 'PropertyController@show',
-  'as' => 'property_show_path'
+    'uses' => 'PropertyController@show',
+    'as' => 'property_show_path'
 ]);
 
 /**
+<<<<<<< HEAD
 * Project routes
 */
 Route::get('projects/crear',[
@@ -73,9 +78,13 @@ Route::delete('projects/{id}/borrar',[
   'uses' => 'ProjectController@destroy',
   'as' => 'projects_delete_path'
 ]);
+=======
+ * Project routes
+ */
+>>>>>>> origin/master
 Route::get('/projects', [
-  'uses' => 'ProjectController@index',
-  'as' => 'projects_path'
+    'uses' => 'ProjectController@index',
+    'as' => 'projects_path'
 ]);
 Route::get('project/{id}',[
   'uses' => 'ProjectController@show',
