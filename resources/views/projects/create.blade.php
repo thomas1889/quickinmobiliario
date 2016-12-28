511caa0d970c62d570832de6d02e1bbf1d484924
Santiago Barrios H <santiagobarrios08@gmail.com>
2016-12-28 15:36:56 -0500

    ProjectController
    Create form

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -5,6 +5,7 @@
 	<div class="col-md-8 col-md-offset-2">
 		<h2>Por favor ingrese todos los datos para <br>registrar su <i>Proyecto</i>.</h2>
 		@include('errors._validation_errors')
+		<form id="form-create-project" class="form" action="{{ route('projects_create_path') }}" method="POST">
 			{{ csrf_field() }}
 			@include('partials._project_form')
 		</form>
