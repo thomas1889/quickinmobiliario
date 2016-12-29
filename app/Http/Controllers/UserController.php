69c0f485444c89c64c6cca69145f64c5b1ac068f
Stivenson <stivenson02@gmail.com>
2016-12-29 16:53:13 -0500

    File Perfil de usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use QuickInmobiliario\Http\Controllers\Controller;
 use QuickInmobiliario\User;
+use Illuminate\Support\Facades\Storage;
 
 //use QuickInmobiliario\User;
 
@@ -20,26 +21,34 @@ class UserController extends Controller {
     }
 
     public function update(Request $data) {
+        dd();
 
-//        $user->first_name = $request->get('first_name');
-//        $user->save();
-        $id = Auth::User()->id;
-        $user_update = User::findOrFail($id);
-        $user_update->username = $data->get('username');
-        $user_update->first_name = $data->get('first_name');
-        $user_update->last_name = $data->get('last_name');
-        $user_update->business_name = $data->get('business_name');
-        $user_update->document_type = $data->get('document_type');
-        $user_update->document = $data->get('document');
-        $user_update->email = $data->get('email');
-        $user_update->cell_phone = $data->get('cell_phone');
-        $user_update->address = $data->get('address');
-        $user_update->age = $data->get('age');
-        $user_update->gender = $data->get('gender');
-        $user_update->profession = $data->get('profession');
-        $user_update->city = $data->get('city');
-        $user_update->save();
-        return view('home');
+        $file = $data->file('image');
+        $rnd = str_random(10);
+        $nombre = $rnd . '-' . $file->getClientOriginalName();
+        $path = public_path('uploads/' . $nombre);
+        $url = '/uploads/' . $nombre;
+
+
+//        $id = Auth::User()->id;
+//        $user_update = User::findOrFail($id);
+//        $user_update->username = $data->get('username');
+//        $user_update->first_name = $data->get('first_name');
+//        $user_update->last_name = $data->get('last_name');
+//        $user_update->business_name = $data->get('business_name');
+//        $user_update->document_type = $data->get('document_type');
+//        $user_update->document = $data->get('document');
+//        $user_update->email = $data->get('email');
+//        $user_update->cell_phone = $data->get('cell_phone');
+//        $user_update->address = $data->get('address');
+//        $user_update->age = $data->get('age');
+//        $user_update->gender = $data->get('gender');
+//        $user_update->profession = $data->get('profession');
+//        $user_update->city = $data->get('city');
+//        $user_update->image_perfil =$url; 
+//        $user_update->save();
+//        return view('home');
+        return'<img src="' . $url . '"/>';
     }
 
 }
