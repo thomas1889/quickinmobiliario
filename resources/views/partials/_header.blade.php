0a65fb8d31ccd450bd0438d9cb446057d20e4482
Jonatan Bonilla <pescadin138@gmail.com>
2017-01-18 11:19:13 -0500

    Modificación en menú principal

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -57,88 +57,9 @@
             <nav id="primary-menu">
               <ul class="main-menu text-center">
                 <li><a href="{{ url('/') }}">Home</a></li>
-                <li class="mega-parent"><a href="#">Elements</a>
-                  <div class="mega-menu-area">
-                    <ul class="single-mega-item">
-                      <li><a href="elements-accordion.html">Accordion</a></li>
-                      <li><a href="elements-agent.html">Agent</a></li>
-                      <li><a href="elements-alerts.html">Alerts</a></li>
-                      <li><a href="elements-audio.html">Audio</a></li>
-                      <li><a href="elements-call-to-actions.html">Call-to-actions</a></li>
-                      <li><a href="elements-contact-1.html">Contact-1</a></li>
-                      <li><a href="elements-contact-2.html">Contact-2</a></li>
-                    </ul>
-                    <ul class="single-mega-item">
-                      <li><a href="elements-features.html">Features</a></li>
-                      <li><a href="elements-footer-1.html">Footer-1</a></li>
-                      <li><a href="elements-footer-2.html">Footer-2</a></li>
-                      <li><a href="elements-header-sticky-no.html">Header-sticky-no</a></li>
-                      <li><a href="elements-header-sticky.html">Header-sticky</a></li>
-                      <li><a href="elements-map.html">Map</a></li>
-                      <li><a href="elements-media.html">Media</a></li>
-                    </ul>
-                    <ul class="single-mega-item">
-                      <li><a href="elements-pagination.html">Pagination</a></li>
-                      <li><a href="elements-progress-bar.html">Progress-bar</a></li>
-                      <li><a href="elements-property.html">Property</a></li>
-                      <li><a href="elements-services.html">Services</a></li>
-                      <li><a href="elements-sidebar-left-1.html">Sidebar-left-1</a></li>
-                      <li><a href="elements-sidebar-left-2.html">Sidebar-left-2</a></li>
-                      <li><a href="elements-sidebar-right-1.html">Sidebar-right-1</a></li>
-                    </ul>
-                    <ul class="single-mega-item">
-                      <li><a href="elements-sidebar-right-2.html">Sidebar-right-1</a></li>
-                      <li><a href="elements-tab.html">Tab</a></li>
-                      <li><a href="elements-table.html">Table</a></li>
-                      <li><a href="elements-testimonials.html">Testimonials</a></li>
-                      <li><a href="elements-toggles.html">Toggles</a></li>
-                      <li><a href="elements-typography.html">Typography</a></li>
-                      <li><a href="elements-video.html">Video</a></li>
-                    </ul>
-                  </div>
-                </li>
-                <li><a href="service.html">Service</a>
-                  <ul class="drop-menu">
-                    <li><a href="service.html">Service</a></li>
-                    <li><a href="service-details.html">Service details</a></li>
-                  </ul>
-                </li>
-                <li><a href="properties.html">Properties</a>
-                  <ul class="drop-menu">
-                    <li><a href="properties.html">Properties</a></li>
-                    <li><a href="properties-left-sidebar.html">Properties left sidebar</a></li>
-                    <li><a href="properties-right-sidebar.html">Properties right sidebar</a></li>
-                    <li><a href="properties-details.html">Properties details</a></li>
-                  </ul>
-                </li>
-                <li><a href="agent.html">Agent</a>
-                  <ul class="drop-menu">
-                    <li><a href="agent.html">Agent</a></li>
-                    <li><a href="agent-details.html">Agent details</a></li>
-                  </ul>
-                </li>
-                <li><a href="#">Pages's</a>
-                  <ul class="drop-menu menu-right">
-                    <li><a href="about.html">About</a></li>
-                    <li><a href="agent.html">Agent</a></li>
-                    <li><a href="agent-details.html">Agent-details</a></li>
-                    <li><a href="features.html">Features</a></li>
-                    <li><a href="service.html">Service</a></li>
-                    <li><a href="service-details.html">Service details</a></li>
-                    <li><a href="login.html">Login</a></li>
-                    <li><a href="404.html">404</a></li>
-                    <li><a href="contact.html">Contact</a></li>
-                  </ul>
-                </li>
-                <li><a href="blog.html">Blog</a>
-                  <ul class="drop-menu menu-right">
-                    <li><a href="blog.html">Blog</a></li>
-                    <li><a href="blog-left-sidebar.html">Blog left sidebar</a></li>
-                    <li><a href="blog-right-sidebar.html">Blog right sidebar</a></li>
-                    <li><a href="single-blog.html">Single Bloge</a></li>
-                  </ul>
-                </li>
-                <li><a href="contact.html">Contact</a></li>
+                <li><a href="#">Publicar</a></li>
+                <li><a href="#">Acelerador</a></li>
+                <li><a href="#">Contact</a></li>
               </ul>
             </nav>
           </div>
