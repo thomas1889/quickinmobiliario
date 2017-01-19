c3a772d5f8933efe6e1eba8471c79b0902ba06dc
Santiago Barrios H <santiagobarrios08@gmail.com>
2017-01-19 12:16:48 -0500

    Model factory modified users into project's table
    Model factory modified users into project's table

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -93,6 +93,7 @@ $factory->define(QuickInmobiliario\Project::class, function (Faker\Generator $fa
       'use_type_id' => $faker->randomElement([1, 2]),
       'business_type_id' => $faker->randomElement([1, 2, 3, 4]),
       'video360' => '-xNN-bJQ4vI',
+      'user_id' => 4,
       'coordinates' => DB::raw("GeomFromText('POINT(18 -64)')"),
     ];
 });
