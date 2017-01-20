2a2b29caaaeab4ae17fb8de56dd35a1ba15675a1
Santiago Barrios H <santiagobarrios08@gmail.com>
2017-01-20 17:17:36 -0500

    Faq's updated
    Faq's updated

--- resources/views/faqs.blade.php
+++ resources/views/faqs.blade.php
@@ -30,15 +30,15 @@
 				<div class="row">
 					<!-- starts the 1st toogle  -->
 					<div class="col-md-6">
-						<h5 class="mb-50"><strong>1-</strong> Período Promocional</h5>
+						<h3 class="form-division mb-30">1- Período Promocional</h3>
 						<div class="panel-group" id="accordion_1">
 							<div class="panel panel-default">
 								<div class="panel-heading" id="headingOne">
-									<h4 class="panel-title">
+									<h5 class="panel-title">
 										<a data-toggle="collapse" data-parent="#accordion_1" href="#collapseOne_1" aria-expanded="true">
-											<strong>a)</strong> ¿Cuántos días dura el período promocional?
+											<i class="fa fa-bolt" aria-hidden="true"></i> ¿Cuántos días dura el período promocional?
 										</a>
-									</h4>
+									</h5>
 								</div>
 								<div id="collapseOne_1" class="panel-collapse collapse in" >
 									<div class="panel-body">
@@ -48,25 +48,25 @@
 							</div>
 							<div class="panel panel-default">
 								<div class="panel-heading" id="headingTwo">
-									<h4 class="panel-title">
+									<h5 class="panel-title">
 										<a class="collapsed" data-toggle="collapse" data-parent="#accordion_1" href="#collapseTwo_1" aria-expanded="false">
-											<strong>b)</strong> ¿Tengo que instalar algún software para usar QuickInmobiliario?
+											<i class="fa fa-bolt" aria-hidden="true"></i> ¿Tengo que instalar algún software para usar QuickInmobiliario?
 										</a>
-									</h4>
+									</h5>
 								</div>
 								<div id="collapseTwo_1" class="panel-collapse collapse">
 									<div class="panel-body">
-									No, en lo absoluto, QuickInmobiliario es un servicio 100% en la nube, es decir, ingresar a <a href="#">QuickInmobiliario.com</a> es el único requisito.
+										No, en lo absoluto, QuickInmobiliario es un servicio 100% en la nube, es decir, ingresar a <a href="#">QuickInmobiliario.com</a> es el único requisito.
 									</div>
 								</div>
 							</div>
 							<div class="panel panel-default">
 								<div class="panel-heading" id="headingThree">
-									<h4 class="panel-title">
+									<h5 class="panel-title">
 										<a class="collapsed" data-toggle="collapse" data-parent="#accordion_1" href="#collapseThree_1" aria-expanded="false">
-											<strong>c)</strong> ¿Tengo que proveer datos de pago para usar QuickInmobiliario?
+											<i class="fa fa-bolt" aria-hidden="true"></i> ¿Tengo que proveer datos de pago para usar QuickInmobiliario?
 										</a>
-									</h4>
+									</h5>
 								</div>
 								<div id="collapseThree_1" class="panel-collapse collapse"  aria-labelledby="headingThree">
 									<div class="panel-body">
@@ -76,11 +76,11 @@
 							</div>
 							<div class="panel panel-default">
 								<div class="panel-heading" id="headingFour">
-									<h4 class="panel-title">
+									<h5 class="panel-title">
 										<a class="collapsed" data-toggle="collapse" data-parent="#accordion_1" href="#collapseFour_1" aria-expanded="false">
-											<strong>d)</strong> ¿Luego del período promocional, me cobrarán?
+											<i class="fa fa-bolt" aria-hidden="true"></i> ¿Luego del período promocional, me cobrarán?
 										</a>
-									</h4>
+									</h5>
 								</div>
 								<div id="collapseFour_1" class="panel-collapse collapse"  aria-labelledby="headingFour">
 									<div class="panel-body">
@@ -89,26 +89,12 @@
 								</div>
 							</div>
 							<div class="panel panel-default">
-								<div class="panel-heading" id="headingFive">
-									<h4 class="panel-title">
-										<a class="collapsed" data-toggle="collapse" data-parent="#accordion_1" href="#collapseFive_1" aria-expanded="false">
-											<strong>e)</strong> ¿Luego del período promocional, me cobrarán?
-										</a>
-									</h4>
-								</div>
-								<div id="collapseFive_1" class="panel-collapse collapse"  aria-labelledby="headingFive">
-									<div class="panel-body">
-										No, no realizamos cargo de ningún tipo al vencer tu período promocional a menos de que solicites la activación de cualquiera de nuestros planes.
-									</div>
-								</div>
-							</div>
-							<div class="panel panel-default">
 								<div class="panel-heading" id="headingSix">
-									<h4 class="panel-title">
+									<h5 class="panel-title">
 										<a class="collapsed" data-toggle="collapse" data-parent="#accordion_1" href="#collapseSix_1" aria-expanded="false">
-											<strong>f)</strong> ¿Cuáles funcionalidades están activas durante el período promocional?
+											<i class="fa fa-bolt" aria-hidden="true"></i> ¿Cuáles funcionalidades están activas durante el período promocional?
 										</a>
-									</h4>
+									</h5>
 								</div>
 								<div id="collapseSix_1" class="panel-collapse collapse"  aria-labelledby="headingSix">
 									<div class="panel-body">
@@ -121,60 +107,103 @@
 					<!-- ending 1st toogle  -->
 					<!-- starts the 2nd toogle -->
 					<div class="col-md-6">
-						<h5 class="mb-50"><strong>2-</strong>Elements Toggles style-2</h5>
+						<h3 class="form-division mb-30">2- Pagos.</h3>
 						<div class="panel-group" id="accordion_2">
 							<div class="panel panel-default">
 								<div class="panel-heading" id="headingOne">
-									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordion_1" href="#collapseOne_2" aria-expanded="true">
-											Collapsible Group Item #1
+									<h5 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordion_2" href="#collapseOne_2" aria-expanded="true">
+											<i class="fa fa-bolt" aria-hidden="true"></i> ¿Los precios incluyen IVA, y entregan factura?
 										</a>
-									</h4>
+									</h5>
 								</div>
 								<div id="collapseOne_2" class="panel-collapse collapse in" >
 									<div class="panel-body">
-										Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+										Sí, Los precios ya incluyen IVA, y la factura es eléctronica.
 									</div>
 								</div>
 							</div>
 							<div class="panel panel-default">
 								<div class="panel-heading" id="headingTwo">
-									<h4 class="panel-title">
+									<h5 class="panel-title">
 										<a class="collapsed" data-toggle="collapse" data-parent="#accordion_2" href="#collapseTwo_2" aria-expanded="false">
-											Collapsible Group Item #2
+											<i class="fa fa-bolt" aria-hidden="true"></i> ¿Cuáles monedas aceptan?
 										</a>
-									</h4>
+									</h5>
 								</div>
 								<div id="collapseTwo_2" class="panel-collapse collapse">
 									<div class="panel-body">
-										Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+										Aceptamos sólamente Pesos Colombianos (COP).
 									</div>
 								</div>
 							</div>
 							<div class="panel panel-default">
 								<div class="panel-heading" id="headingThree">
-									<h4 class="panel-title">
+									<h5 class="panel-title">
 										<a class="collapsed" data-toggle="collapse" data-parent="#accordion_2" href="#collapseThree_2" aria-expanded="false">
-											Collapsible Group Item #3
+											<i class="fa fa-bolt" aria-hidden="true"></i> ¿Cuál es el procedimiento para contratar?
 										</a>
-									</h4>
+									</h5>
 								</div>
 								<div id="collapseThree_2" class="panel-collapse collapse"  aria-labelledby="headingThree">
 									<div class="panel-body">
-										Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+										Es muy sencillo, sólo sigue estos pasos:
+										<ol>
+											<li>Completa tu proceso de <a href="">registro</a> o <a href="">inicia sesión</a> si ya eres usuario.</li>
+											<li>Una vez dentro, dirígete la sección <a href="">Precios</a></li>
+											<li>Asegúrate que estés en la pestaña Pedidos, luego haz click en Seleccionar y Contratar Plan</li>
+											<li>Selecciona el método de pago de tu preferencia y ¡listo!</li>
+										</ol>
+									</div>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading" id="headingFour">
+									<h5 class="panel-title">
+										<a class="collapsed" data-toggle="collapse" data-parent="#accordion_2" href="#collapseFour_2" aria-expanded="false">
+											<i class="fa fa-bolt" aria-hidden="true"></i> ¿Cuáles son los métodos de pago aceptados?
+										</a>
+									</h5>
+								</div>
+								<div id="collapseFour_2" class="panel-collapse collapse"  aria-labelledby="headingFour">
+									<div class="panel-body">
+										<ol>
+											<!-- <li>Para pagos desde el exterior aceptamos tarjeta de crédito a través de Paypal</li> -->
+											<li>Para Colombia, aceptamos depósito o transferencia bancaria, de igual forma, aceptamos pagos con tarjeta de crédito a través de <a href="https://www.mercadopago.com.co" target="_blank">MercadoPago</a></li>
+										</ol>
+									</div>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading" id="headingFive">
+									<h5 class="panel-title">
+										<a class="collapsed" data-toggle="collapse" data-parent="#accordion_2" href="#collapseFive_2" aria-expanded="false">
+											<i class="fa fa-bolt" aria-hidden="true"></i> ¿Ofrecen descuentos en sus planes?
+										</a>
+									</h5>
+								</div>
+								<div id="collapseFive_2" class="panel-collapse collapse"  aria-labelledby="headingFive">
+									<div class="panel-body">
+										Sí, ofrecemos descuentos por pagos adelantados así:
+										<ol>
+											<li>Trimestral
+												<li>semestral </li>
+												<li>y anual con el 8% </li>
+											</ol>
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
+						<!-- ending 2nd toogle -->
 					</div>
 				</div>
 			</div>
+			<!-- ELEMENTS AREA END -->
+
+			@include('partials._newsletter')
+			<!-- SUBSCRIBE AREA END -->
 		</div>
-		<!-- ELEMENTS AREA END -->
+		<!-- End page content -->
 
-		@include('partials._newsletter')
-		<!-- SUBSCRIBE AREA END -->
-	</div>
-	<!-- End page content -->
-
-	@stop
+		@stop
