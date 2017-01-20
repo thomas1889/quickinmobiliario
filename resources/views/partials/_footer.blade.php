d59d3c5144605e8d10bda886cf2555ac407a27f7
Santiago Barrios H <santiagobarrios08@gmail.com>
2017-01-20 15:59:58 -0500

    translation for newsletter
    translation for newsletter

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -3,9 +3,9 @@
 		<div class="container">
 			<div class="row">
 				<!-- footer-address -->
-				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
 					<div class="footer-widget">
-						<h6 class="footer-titel">Contáctenos</h6>
+						<h6 class="footer-titel">Ubicación</h6>
 						<ul class="footer-address">
 							<li>
 								<div class="address-icon">
@@ -31,24 +31,24 @@
 								</div>
 								<div class="address-info">
 									<span>Email : info@quickinmobiliario.com</span>
-									<span>Skype :<a href="#" target="_blank"> quickinmobiliario.</a></span>
+									<span>Skype :<a href="#" target="_blank">quickinmobiliario</a></span>
 								</div>
 							</li>
 							<li>
-								<div class="latest-news-info">
-									<div class="">
+
+									<div class="socialnet">
 									<span><a href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true" title="Facebook"></i></a></span>
 									<span><a href="#"><i class="fa fa-instagram fa-2x" aria-hidden="true" title="Instagram"></i></a></span>
 									<span><a href="#"><i class="fa fa-youtube-play fa-2x" aria-hidden="true" title="YouTube"></i></a></span>
 									<span><a href="#"><i class="fa fa-newspaper-o fa-2x" aria-hidden="true" title="Blog"></i></a></span>
 								</div>
-							</div>
+
 							</li>
 						</ul>
 					</div>
 				</div>
 				<!-- footer-latest-news -->
-				<div class="col-lg-6 col-md-5 hidden-sm col-xs-12">
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
 					<div class="footer-widget middle">
 						<h6 class="footer-titel">Más Opciones</h6>
 						<ul class="footer-latest-news">
@@ -63,62 +63,58 @@
 							</li>
 							<li>
 								<div class="latest-news-info">
-									<h6>
-										<a href="">
-											<i class="fa fa-pencil" aria-hidden="true"></i>&nbsp;&nbsp;Publicar Inmueble
-										</a>
-									</h6>
+									<h6><a href="">
+									<i class="fa fa-pencil" aria-hidden="true"></i>&nbsp;&nbsp;Publicar Inmueble
+									</a></h6>
 								</div>
 							</li>
 							<li>
 								<div class="latest-news-info">
 									<h6>
 										<a href="">
-											<i class="fa fa-pencil" aria-hidden="true"></i>&nbsp;&nbsp;Publicar Proyecto
-										</a>
-									</h6>
+										<i class="fa fa-pencil" aria-hidden="true"></i>&nbsp;&nbsp;Publicar Proyecto
+									</a>
+								</h6>
 								</div>
 							</li>
 							<li>
 								<div class="latest-news-info">
 									<h6>
 										<a href="">
-											<i class="fa fa-universal-access" aria-hidden="true"></i>&nbsp;&nbsp;Acelerador
-										</a>
-									</h6>
+										<i class="fa fa-universal-access" aria-hidden="true"></i>&nbsp;&nbsp;Acelerador
+									</a>
+								</h6>
 								</div>
 							</li>
 							<li>
 								<div class="latest-news-info">
-									<h6>
-										<a href="">
-											<i class="fa fa-bell" aria-hidden="true"></i>&nbsp;&nbsp;Contactenos
-										</a>
-									</h6>
+									<h6><a href="">
+										<i class="fa fa-bell" aria-hidden="true"></i>&nbsp;&nbsp;Contactenos
+									</a>
+								</h6>
 								</div>
 							</li>
 							<li>
-								<div class="">
+								<div class="latest-news-info">
 									<h6>
 										<a href="{{ url('preguntasfrecuentes') }}">
 											<i class="fa fa-life-ring" aria-hidden="true"></i>&nbsp;&nbsp;Preguntas Frecuentes
-										</a>
-									</h6>
+									</a></h6>
 								</div>
 							</li>
 						</ul>
 					</div>
 				</div>
 				<!-- footer-contact -->
-				<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
 					<div class="footer-widget">
-						<h6 class="footer-titel">QUICK CONTACT</h6>
+						<h6 class="footer-titel">QUICK Contácto</h6>
 						<div class="footer-contact">
-							<p>Sí tienes alguna duda con respecto al servico, queja o sugerencia, escribenos:</p>
+							<p>Sí tienes alguna duda, queja o sugerncia, puedes escribirnos</p>
 							<form  id="contact-form-2" action="mail_footer.php" method="post">
-								<input type="email" name="email2" placeholder="Tu correo electrónico...">
-								<textarea name="message2" placeholder="Tu mensaje..."></textarea>
-								<button type="submit" value="send">Enviar</button>
+								<input type="email" name="email2" placeholder="Dirección de E-mail ...">
+								<textarea name="message2" placeholder="Su Mensaje..."></textarea>
+								<button type="submit" value="send">Envíar</button>
 							</form>
 							<p class="form-messege"></p>
 						</div>
@@ -132,7 +128,7 @@
 			<div class="row">
 				<div class="col-xs-12">
 					<div class="copyright text-center">
-						<p>Todos los derechos son reservados. <b>Quick Inmobiliario</b>.&copy; 2016. </p>
+						<p>Todos los derechos son reservados,<b> Quick Inmobiliario &copy;, 2016</b>.</p>
 					</div>
 				</div>
 			</div>
--- resources/views/partials/_newsletter.blade.php
+++ resources/views/partials/_newsletter.blade.php
@@ -4,15 +4,15 @@
     <div class="row">
       <div class="col-md-3 col-sm-4 col-xs-12">
         <div class="section-title text-white">
-          <h3>SUBSCRIBE</h3>
-          <h2 class="h1">NEWSLETTER</h2>
+          <h3>Suscribete a</h3>
+          <h2 class="h1">Nuestras Noticias</h2>
         </div>
       </div>
       <div class="col-md-9 col-sm-8 col-xs-12">
         <div class="subscribe">
           <form action="#">
-            <input type="text" name="subscribe" placeholder="Enter yur email here...">
-            <button type="submit" value="send">Send</button>
+            <input type="text" name="subscribe" placeholder="Tu dirección de email...">
+            <button type="submit" value="send">Suscribirme</button>
           </form>
         </div>
       </div>
