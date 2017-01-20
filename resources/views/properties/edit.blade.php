acbcb27061de8058889ba0bb88cd8877cf5ed047
Jonatan Bonilla <pescadin138@gmail.com>
2017-01-20 14:58:53 -0500

    Modificaciones en el diseño del formulario crear inmueble

--- resources/views/properties/edit.blade.php
+++ resources/views/properties/edit.blade.php
@@ -43,31 +43,32 @@
   <section id="page-content" class="page-wrapper">
 
     <!-- PROPERTIES DETAILS AREA START -->
-    <div class="properties-details-area pt-115 pb-60">
+    <div class="properties-details-area pt-50 pb-60">
       <div class="container">
         <div class="row">
-          <div class="col-md-8">
-            <h1>Editar Inmueble <small class="pull-right"><a href="{{ route('properties_path') }}" class="btn btn-link"><< Regresar</a></small></h1>
+          <div class="col-md-12">
+            <div class="contact-messge edition bg-gray">
+              <!-- blog-details-reply -->
+              <div class="leave-review form-edition">
+                <!--<h1>Editar Inmueble <small class="pull-right"><a href="{{ route('properties_path') }}" class="btn btn-link"><< Regresar</a></small></h1>-->
 
-            @include('errors._validation_errors')
+                @include('errors._validation_errors')
 
-            <form class="form" id="form-edit-property" action="{{ route('property_edit_path', $property->id) }}" method="post">
-              {{ csrf_field() }}
-              {{ method_field('PUT') }}
-              @include('partials._property_form')
-            </form>
-            <uploadimages form="update-property-images" principal="form-edit-property" title="Imágenes inmueble" save="{{ route('property_image_upload_path') }}" delete="/imagen_inmueble" token="{{ csrf_token() }}"></uploadimages>
-            <div class="form-group">
-              <button type="button" class="btn btn-primary" v-on:click="updateProperty">Actualizar Inmueble</button>
+                <form class="form" id="form-edit-property" action="{{ route('property_edit_path', $property->id) }}" method="post">
+                  {{ csrf_field() }}
+                  {{ method_field('PUT') }}
+                  @include('partials._property_form')
+                </form>
+                <uploadimages form="update-property-images" principal="form-edit-property" title="Imágenes inmueble" save="{{ route('property_image_upload_path') }}" delete="/imagen_inmueble" token="{{ csrf_token() }}"></uploadimages>
+                <div class="form-group">
+                  <button type="button" class="submit-btn-1" v-on:click="updateProperty">Actualizar Inmueble</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-
-    <!-- NEWSLETTER AREA START -->
-    @include('partials._newsletter')
-    <!-- NEWSLETTER AREA END -->
   </section>
 
 @stop
