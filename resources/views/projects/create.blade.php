8d0537f94982efaa94ac2d96075a8ba2baf0fce4
Santiago Barrios H <santiagobarrios08@gmail.com>
2017-01-23 23:29:52 -0500

    Updating project's layout
    Updating project's layout

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,25 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-	<div class="col-md-8 col-md-offset-2">
-		<h1 class="text-center">
-			<a href="{{ route('projects_path' )}}" class="btn btn-info pull-left"> Volver</a>
-			<a href="{{ route('projects_create_path') }}" class="btn btn-success pull-right">
-				Publicar un Proyecto
-			</a>
-			<br><br>
-			Por favor ingrese todos los datos para <br>registrar su <i>Proyecto</i>.
-		</h1><hr>
-		@include('errors._validation_errors')
+
+<!-- BREADCRUMBS AREA START -->
+  <div class="breadcrumbs-area bread-bg-1 bg-opacity-black-70">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="breadcrumbs">
+            <h2 class="breadcrumbs-title">{{ $section_text }} Proyectos</h2>
+            <ul class="breadcrumbs-list">
+              <li><a href="{{ url('/') }}">Inicio</a></li>
+              <li><a href="{{ route('proyectos_path') }}">Proyectos</a></li>
+              <li>{{ $section_text }} Proyectos</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- BREADCRUMBS AREA END -->
+
+ <!-- Start page content -->
+  <section id="page-content" class="page-wrapper">
+
+    <!-- PROPERTIES DETAILS AREA START -->
+    <div class="properties-details-area pt-50 pb-60">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="contact-messge edition bg-gray">
+              <div class="leave-review form-edition">
+                @include('errors._validation_errors')
 		<form id="form-create-project" class="form" action="{{ route('projects_create_path') }}" method="POST">
 			{{ csrf_field() }}
 			@include('partials._project_form')
 		</form>
-		<uploadimages form="my-awesome-dropzone" principal="form-create-project" title="Imágenes del Proyecto" save="{{ route('project_image_upload_path') }}" delete="/imagen_proyecto" token="{{ csrf_token() }}"></uploadimages>
-		<div class="form-group ">
-			<button type="button" class="btn btn-primary" v-on:click="createProject">Guardar Cambios</button>
-		</div>
-	</div>
-</div>
+                
+                <div class="form-group">
+                  <button type="button" class="submit-btn-1" v-on:click="createProject">
+                    Guardar Cambios.
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
 @stop
